cbc8c0f569216631208c5cae484066e2d7b7375b
“gacorrei” <gguerrafranco@gmail.com>
2023-07-26 15:00:31 +0100

    Getting there

--- CPP00/ex01/PhoneBook.cpp
+++ CPP00/ex01/PhoneBook.cpp
@@ -6,7 +6,7 @@
 /*   By: gacorrei <gacorrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/25 10:04:27 by gacorrei          #+#    #+#             */
-/*   Updated: 2023/07/25 15:21:43 by gacorrei         ###   ########.fr       */
+/*   Updated: 2023/07/26 14:57:44 by gacorrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int	strncmp(std::string str1, std::string str2)
 {
 	int	i = 0;
 
-	for (i; str1[i]; i++)
+	for ( ; str1[i]; i++)
 		if (str1[i] != str2[i])
 			break;
 	return ((unsigned char)str1[i] - (unsigned char)str2[i]);
@@ -26,7 +26,7 @@ int	strlen(std::string str)
 {
 	int	i = 0;
 
-	for (i; str[i]; i++)
+	for ( ; str[i]; i++)
 		continue;
 	return (i);
 }
@@ -35,6 +35,8 @@ int	check_string(const std::string check)
 {
 	int	i;
 
+	if (check.empty())
+		return (1);
 	for(i = 0; check[i]; i++)
 		if (!std::isalpha(check[i]))
 			return (1);
@@ -45,7 +47,11 @@ int	check_string(const std::string check)
 
 int	check_number(const std::string check)
 {
-	for(int i = 0; check[i]; i++)
+	int i;
+
+	if (check.empty())
+		return (1);
+	for(i = 0; check[i]; i++)
 		if (!std::isdigit(check[i]))
 			return (1);
 	return (0);
@@ -62,6 +68,7 @@ void	print_column(std::string info)
 		align = 10 - len;
 		for (int i = 0; i < align; i++)
 			std::cout << " ";
+		std::cout << info;
 	}
 	else if (len > 10)
 	{
@@ -74,8 +81,21 @@ void	print_column(std::string info)
 	return;
 }
 
+void	print_separator(int flag)
+{
+	if (flag)
+		std::cout << "|     Index|First Name| Last Name|  Nickname|\n";
+	for (int i = 0; i < 45; i++)
+		std::cout << "-";
+	std::cout << std::endl;
+	return;
+}
+
 void	print_contact(Contact contact, int index)
 {
+	print_separator(0);
+	print_separator(1);
+	std::cout << "|";
 	std::cout << "         ";
 	std::cout << index;
 	std::cout << "|";
@@ -84,12 +104,18 @@ void	print_contact(Contact contact, int index)
 	print_column(contact.get_last_name());
 	std::cout << "|";
 	print_column(contact.get_nickname());
-	std::cout << std::endl;
+	std::cout << "|\n";
+	print_separator(0);
 }
 
 int		PhoneBook::get_current_size(void) const
 {
 	return (this->current_size);
+}
+
+int		PhoneBook::get_index(void) const
+{
+	return (this->index);
 }
 
 void	PhoneBook::add_contact(void)
@@ -102,27 +128,27 @@ void	PhoneBook::add_contact(void)
 
 	while (check_string(first_name))
 	{
-		std::cout << "Input contact's first name\n";
+		std::cout << "Input contact's first name: ";
 		std::cin >> first_name;
 	}
 	while (check_string(last_name))
 	{
-		std::cout << "Input contact's last name\n";
+		std::cout << "Input contact's last name: ";
 		std::cin >> last_name;		
 	}
 	while (check_string(nickname))
 	{
-		std::cout << "Input contact's nickname\n";
+		std::cout << "Input contact's nickname: ";
 		std::cin >> nickname;		
 	}
-	while (check_string(phone_number) || check_number(phone_number))
+	while (check_number(phone_number))
 	{
-		std::cout << "Input contact's phone number\n";
+		std::cout << "Input contact's phone number: ";
 		std::cin >> phone_number;		
 	}
 	while (check_string(darkest_secret))
 	{
-		std::cout << "Input contact's darkest secret\n";
+		std::cout << "Input contact's darkest secret: ";
 		std::cin >> darkest_secret;		
 	}
 	this->contacts[this->index].set_first_name(first_name);
@@ -139,43 +165,54 @@ void	PhoneBook::search_contact(void) const
 {
 	int	index;
 	
-	for (int i = 0; i < get_current_size(); i++)
+	for (int i = 0; i < this->get_current_size(); i++)
 		print_contact(this->contacts[i], i);
-	while (1)
+	std::cout << "Choose index to display: ";
+	std::cin >> index;
+	if (std::cin.eof())
 	{
-		std::cout << "Choose index to display: ";
-		std::cin >> index;
-		std::cout << std::endl;
-		if (index < 0 || index > 7)
-			std::cout << "Input an index in range [0 - 7]\n";
-		else if (index > get_current_size() - 1)
-			std::cout << "There is no contact for selected index\n";
-		else
-			break;
+		std::cin.clear();
+		return;
 	}
-	print_contact(this->contacts[index], index);
+	std::cout << std::endl;
+	if (std::cin.fail() || index < 0 || index >= this->get_current_size())
+	{
+		std::cout << "Input an index in range [0 - " << this->get_current_size() - 1 <<"]\n";
+		// std::cin.clear();
+		return;
+	}
+	else if (index > get_current_size() - 1)
+		std::cout << "There is no contact for selected index\n";
+	std::cout << "First Name: " << contacts[index].get_first_name() << std::endl ;
+	std::cout << "Last Name: " << contacts[index].get_last_name() << std::endl ;
+	std::cout << "Nickname: " << contacts[index].get_nickname() << std::endl ;
+	std::cout << "Phone Number: " << contacts[index].get_phone_number() << std::endl ;
+	std::cout << "Darkest Secret: " << contacts[index].get_darkest_secret() << std::endl ;
 	return;
 }
 
 int	main(void)
 {
 	std::string input;
+	PhoneBook	phonebook;
 
+	//Tests for what is in phonebook at start
+	std::cout << "Phonebook size is: " << phonebook.get_current_size() << std::endl;
+	std::cout << "Phonebook index is: " << phonebook.get_index() << std::endl;
+	std::cout << std::endl;
 	while (1)
 	{
-		std::cout << "PhoneBook: Choose one:\n[ADD]\n[SEARCH]\n[EXIT]\n";
+		std::cout << "PhoneBook:\nChoose one: [ADD] [SEARCH] [EXIT]\n>";
 		std::cin >> input;
+		if (input.empty())
+			break ;
 		if (!strncmp(input, "ADD"))
-		{
-			//
-			break ;
-		}
+			phonebook.add_contact();
 		else if (!strncmp(input, "SEARCH"))
-		{
-			//
-			break ;
-		}
+			phonebook.search_contact();
 		else if (!strncmp(input, "EXIT"))
 			break;
+		input.clear();
+		std::cout << std::endl;
 	}
 }
--- CPP00/ex01/PhoneBook.hpp
+++ CPP00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: gacorrei <gacorrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/24 15:59:27 by gacorrei          #+#    #+#             */
-/*   Updated: 2023/07/25 14:40:14 by gacorrei         ###   ########.fr       */
+/*   Updated: 2023/07/26 13:54:42 by gacorrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,6 +27,7 @@ class PhoneBook
 		//Constructor with explicit initialization of current_size
 		PhoneBook() : current_size(0), index(0) {}
 		int		get_current_size(void) const;
+		int		get_index(void) const;
 		void	add_contact(void);
 		void	search_contact(void) const;
 };
@@ -35,6 +36,7 @@ int		strlen(std::string str);
 int		check_string(const std::string check);
 int		check_number(const std::string check);
 void	print_column(std::string info);
+void	print_separator(int flag);
 void	print_contact(Contact contact, int index);
 
 #endif
