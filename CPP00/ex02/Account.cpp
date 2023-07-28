0c784ac72a60a94fe2ab6f3ea944c5041d5ef042
“gacorrei” <gguerrafranco@gmail.com>
2023-07-28 12:08:55 +0100

    Fixed oopsie

--- CPP00/ex02/Account.cpp
+++ CPP00/ex02/Account.cpp
@@ -6,7 +6,7 @@
 /*   By: gacorrei <gacorrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/28 10:32:55 by gacorrei          #+#    #+#             */
-/*   Updated: 2023/07/28 12:06:24 by gacorrei         ###   ########.fr       */
+/*   Updated: 2023/07/28 12:08:41 by gacorrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -87,7 +87,7 @@ static void	_displayTimestamp( void )
 	//Passes current time to tm struct format (as local time)
 	time = std::localtime(&base_time);
 	//Formats output using tm struct fields
-	strftime(timestamp, TIMESTAMP, "[%Y%m%d_%H%M%S]", time);
+	std::strftime(timestamp, TIMESTAMP, "[%Y%m%d_%H%M%S]", time);
 	std::cout << timestamp << std::endl;
 	return (0);
 }
