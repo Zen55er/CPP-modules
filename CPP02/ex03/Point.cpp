796bf01bfe56cdb2e18ebe8e03bcec67863eefc7
“gacorrei” <gguerrafranco@gmail.com>
2023-08-09 14:19:10 +0100

    Minifix

--- CPP02/ex03/Point.cpp
+++ CPP02/ex03/Point.cpp
@@ -6,7 +6,7 @@
 /*   By: gacorrei <gacorrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/08 12:49:45 by gacorrei          #+#    #+#             */
-/*   Updated: 2023/08/09 12:58:02 by gacorrei         ###   ########.fr       */
+/*   Updated: 2023/08/09 14:18:30 by gacorrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ Point::Point(const float fx, const float fy): _x(fx), _y(fy) {}
 
 Point::Point(const Point &copy): _x(copy.get_x()), _y(copy.get_y()) {}
 
-Point::~Point() {}
+Point::~Point(void) {}
 
 Point &Point::operator=(Point copy)
 {
