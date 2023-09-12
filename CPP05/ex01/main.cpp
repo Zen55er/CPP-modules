62600ad6862c52dc28f373573728f46fdd675691
“gacorrei” <gguerrafranco@gmail.com>
2023-09-12 09:13:40 +0100

    added missing tests for bad form creation

--- CPP05/ex01/main.cpp
+++ CPP05/ex01/main.cpp
@@ -6,16 +6,16 @@
 /*   By: gacorrei <gacorrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 14:49:59 by gacorrei          #+#    #+#             */
-/*   Updated: 2023/09/12 08:49:08 by gacorrei         ###   ########.fr       */
+/*   Updated: 2023/09/12 09:12:38 by gacorrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Bureaucrat.hpp"
 
-int	bureau_life(std::string name, int grade, std::string name2, int grade2)
+int	bureau_life(std::string name, int grade, std::string name2, int grade2, int grade3)
 {
 	Bureaucrat	desk_jockey(name, grade);
-	Form		paperwork(name2, grade2, 75);
+	Form		paperwork(name2, grade2, grade3);
 
 	std::cout << desk_jockey << std::endl;
 	std::cout << paperwork << std::endl;
@@ -26,11 +26,11 @@ int	bureau_life(std::string name, int grade, std::string name2, int grade2)
 	return (0);
 }
 
-int	error_testing(std::string name, int grade, std::string name2, int grade2)
+int	error_testing(std::string name, int grade, std::string name2, int grade2, int grade3)
 {
 	try
 	{
-		bureau_life(name, grade, name2, grade2);
+		bureau_life(name, grade, name2, grade2, grade3);
 	}
 	catch(Form::GradeTooHighException &error)
 	{
@@ -62,10 +62,18 @@ int	error_testing(std::string name, int grade, std::string name2, int grade2)
 
 int	main(void)
 {
-	error_testing("pencil_pusher", 74, "form1", 75);
+	error_testing("pencil_pusher", 74, "form1", 0, 75);
 	std::cout << std::endl;
-	error_testing("pencil_pusher", 75, "form1", 75);
+	error_testing("pencil_pusher", 74, "form1", 151, 75);
 	std::cout << std::endl;
-	error_testing("pencil_pusher", 75, "form1", 74);
+	error_testing("pencil_pusher", 74, "form1", 75, 0);
+	std::cout << std::endl;
+	error_testing("pencil_pusher", 74, "form1", 75, 151);
+	std::cout << std::endl;
+	error_testing("pencil_pusher", 74, "form1", 75, 75);
+	std::cout << std::endl;
+	error_testing("pencil_pusher", 75, "form1", 75, 75);
+	std::cout << std::endl;
+	error_testing("pencil_pusher", 75, "form1", 74, 75);
 	return (0);
 }
