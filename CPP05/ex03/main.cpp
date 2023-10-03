91177043c6529c6ddd7412ffb51fd9f401e2d42d
“gacorrei” <gguerrafranco@gmail.com>
2023-10-03 15:24:58 +0100

    FIXED LEAKS AFTER EVAL

--- CPP05/ex03/main.cpp
+++ CPP05/ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: gacorrei <gacorrei@student.42lisboa.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/07 14:49:59 by gacorrei          #+#    #+#             */
-/*   Updated: 2023/09/14 08:48:55 by gacorrei         ###   ########.fr       */
+/*   Updated: 2023/10/03 15:22:36 by gacorrei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,34 +16,34 @@
 #include "PresidentialPardonForm.hpp"
 #include "Intern.hpp"
 
-int	company_life(std::string name, int grade, std::string form, std::string target)
+int	company_life(std::string name, int grade, std::string form, std::string target, AForm **new_form)
 {
 	Bureaucrat	desk_jockey(name, grade);
 	Intern		fng;
-	AForm		*new_form;
 
-	new_form = fng.makeForm(form, target);
-	new_form->beSigned(desk_jockey);
-	new_form->execute(desk_jockey);
+	*new_form = fng.makeForm(form, target);
+	(*new_form)->beSigned(desk_jockey);
+	(*new_form)->execute(desk_jockey);
 	return (0);
 }
 
 int	error_testing(std::string name, int grade, std::string form, std::string target)
 {
+	AForm		*new_form = NULL;
 	try
 	{
-		company_life(name, grade, form, target);
+		company_life(name, grade, form, target, &new_form);
 	}
 	catch(Intern::InvalidFormNameException &error)
 	{
 		std::cerr << error.what() << std::endl;
-		return (1);
 	}
 	catch (...)
 	{
 		std::cerr << "Exception caught" << std::endl;
-		return (1);
 	}
+	if (new_form != NULL)
+		delete new_form;
 	return (0);
 }
 
