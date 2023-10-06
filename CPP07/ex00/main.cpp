2bc3872a01bf24da7acd9dc72ed351c72928a237
“gacorrei” <gguerrafranco@gmail.com>
2023-10-06 11:54:16 +0100

    cpp07 ex00 done

---
+++ CPP07/ex00/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gacorrei <gacorrei@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/06 11:47:24 by gacorrei          #+#    #+#             */
+/*   Updated: 2023/10/06 11:52:53 by gacorrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+
+int	main(void)
+{
+	int a = 2;
+	int b = 3;
+	::swap(a, b);
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min(a, b) = " << ::min(a, b) << std::endl;
+	std::cout << "max(a, b) = " << ::max(a, b) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min(c, d) = " << ::min(c, d) << std::endl;
+	std::cout << "max(c, d) = " << ::max(c, d) << std::endl;
+	return 0;
+}
---
+++ CPP07/ex00/whatever.hpp
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   whatever.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gacorrei <gacorrei@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/06 11:41:10 by gacorrei          #+#    #+#             */
+/*   Updated: 2023/10/06 11:53:08 by gacorrei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+#include <iostream>
+
+template <typename T>
+void swap(T &a, T &b)
+{
+	T	temp;
+
+	temp = a;
+	a = b;
+	b = temp;
+}
+
+template <typename T>
+T min(T a, T b)
+{
+	if (a < b)
+		return a;
+	return b;
+}
+
+template <typename T>
+T max(T a, T b)
+{
+	if (a > b)
+		return a;
+	return b;
+}
